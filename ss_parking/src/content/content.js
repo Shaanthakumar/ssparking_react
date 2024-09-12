51959557eef392dceda326c1b53d857fc512a45d
Shaanthakumar <shaanthakumar04012004@gmail.com>
2024-09-12 11:44:01 +0530

    INclude router Homer page

--- ss_parking/src/content/content.js
+++ ss_parking/src/content/content.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Abtus from "../abtus/abtus"; // About Us page component
 import Cntus from "../cntus/cntus"; // Contact Us page component
 import Navbar from "../navbar/navbar"; // Navbar component
-import Home from "../home/home";
+import Homer from "../home/homer";
 
 function Content() {
     return (
@@ -12,7 +12,7 @@ function Content() {
             <Navbar /> {/* Render Navbar so it is displayed on all routes */}
             <Routes>
                 {/* Define all routes */}
-                <Route path="/" element={<Home />} /> {/* Home component */}
+                <Route path="/" element={<Homer />} /> {/* Home component */}
                 <Route path="/abt" element={<Abtus />} /> {/* About Us component */}
                 <Route path="/cnt" element={<Cntus />} /> {/* Contact Us component */}
             </Routes>
