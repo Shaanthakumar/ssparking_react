25e01a64e702c59252a9de9c22e45b0f7b909eeb
Sanvanth <sanvanthramakrishnan@gmail.com>
2024-09-12 12:07:53 +0530

    changed walkin for checking

--- ss_parking/src/walkin/walkin.js
+++ ss_parking/src/walkin/walkin.js
@@ -2,7 +2,9 @@ import React from "react";
 function Walkin()
 {
     return(
-        <></>
+        <>
+        <h1>HIIIIIIIIIIIIIIIIIIIIIIIIIII</h1>
+        </>
     );
 };
 export default Walkin;
